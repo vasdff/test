16e82d43f3af890064bd26dedda585c3289fcb67
酌与风云 <50521027+vasdff@users.noreply.github.com>
2020-10-08 12:49:29 +0800

    test
    第一天

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import './plugins/element.js';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  render: function (h) { return h(App) }
-}).$mount('#app')
+    router,
+    render: function(h) {
+        return h(App);
+    },
+}).$mount('#app');
